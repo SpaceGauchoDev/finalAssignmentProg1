cf7b5c872c7b3ecd0896e0f1ddee367a3c78ca78
Santiago Rodriguez <rodriguezcuore@gmail.com>
2018-11-18 19:29:57 -0300

    Added top 5 functionality

--- Assets/public_html/js/santiagoJs.js
+++ Assets/public_html/js/santiagoJs.js
@@ -3,31 +3,64 @@
 
 
 function mostrarTop5() {
-    /*
-     console.log("mostrarOfertas");
-     console.log(ofertasPreCargadas);
-     console.log(ofertasPreCargadas.length);
-     */
-
-    var txtOferta = "<ul>";
-
-    for (var i = 0; i < ofertasPreCargadas.length; i++) {
-        /*
-         console.log("ITEM: ");
-         console.log(ofertasPreCargadas[i].imageUrl);
-         */
-
-        txtOferta += "<li>";
-        txtOferta += "<img style='width:150px; height:150px;' src='" + ofertasPreCargadas[i].imageUrl + "' >";
-
-        //txtOferta += "<img src='foto.jpg'>";
-
+    console.log("mostrarTop5");
+	var arrayTop5 = getTop5();
+	var txtOferta = "<ul>";
+	
+	$.each(arrayTop5, function( index, value ) {
+		var oferta = getOferta(value);
+		txtOferta += "<li>";
+        txtOferta += "<img style='width:150px; height:150px;' src='" + oferta.imageUrl + "' >";
         txtOferta += "</li>";
-
-    }
-
+	});
     txtOferta += "</ul>";
     $("#contenedorTop5").html(txtOferta);
+}
+
+function getTop5(){
+	var arrayIndexTop5 = {};
+	var arrayTop5byNumber = new Array();
+	
+	for (var i = 0; i < reservasPreCargadas.length; i++) {
+		var oferta = Number(reservasPreCargadas[i].offerId);
+		if (arrayIndexTop5[String(oferta)] == undefined) {
+			arrayIndexTop5[String(oferta)] = 1;
+		} else {
+			arrayIndexTop5[String(oferta)] = arrayIndexTop5[oferta] + 1;
+		}
+    }
+	for (var i = 0; i < 5; i++) {
+        var aux = getHighestFromArray(arrayIndexTop5);
+		arrayTop5byNumber.push(aux);
+		delete arrayIndexTop5[aux];
+    }	
+	return arrayTop5byNumber;
+}
+
+function getHighestFromArray(arr){
+	var highest = -1;
+	var highestPosition = -1;
+	
+	$.each(arr, function( index, value ) {
+		if (value > highest){
+			highest = value;
+			highestPosition = index;
+		}
+	});
+	return highestPosition;
+}
+
+function getOferta(idOferta){
+	var oferta = {};
+	
+	$.each(ofertasPreCargadas, function( index, value ) {
+		
+		if (value.id == idOferta){
+			oferta = value;
+			//console.log(value.id);
+		}
+	});
+	return oferta;
 }
 
 function cargarDestacados(objOfertas) {
